efe8d889b753b6a6a2d2af315a0ee1d39340e88b
Rachel <80181684+rappleit@users.noreply.github.com>
2023-05-09 09:10:30 +0800

    added images

--- src/components/events.js
+++ src/components/events.js
@@ -13,7 +13,7 @@ const Events = () => {
                     <p>A one-month startup springboard that aims to incorporate the rapid prototyping sprint ideology to energise the building process of student startups within SUTD. VIE will play as mentors during the bi-weekly mastermind sessions, providing crucial feedback on startups’ progress on ideation, validation and launch.</p>
                 </div>
                 <div className={eventsCSS.cardImg}>
-                    <img alt="" src="imgPlaceholder.png" />
+                    <img alt="" src="shittystartupstock.jpg" />
                 </div>
                 
             </div>
@@ -24,7 +24,7 @@ const Events = () => {
                     <p>Periodic sharing sessions with notable speakers from entrepreneurship backgrounds and adjacent industries, including VC analysts and executives, startup founders, accelerator and incubator managers, etc. with 4 or more events a year. VIE helps to connect SEVEN with external partners and collaborators to conduct the sharing seminars.</p>
                 </div>
                 <div className={eventsCSS.cardImg}>
-                    <img  alt="" src="imgPlaceholder.png" />
+                    <img  alt="" src="firesidestock.jpg" />
                 </div>
                 
             </div>
@@ -35,7 +35,7 @@ const Events = () => {
                     <p>Deeply insightful focus group sessions with members, joined exclusively by pre-existing and aspiring founders, encouraging personal sharings of their startup journey and process with an emphasis of learning from failure. VIE also connects SEVEN members to their incubating startups’ key founders and executives, allowing free exchange of wisdom within the community.</p>
                 </div>
                 <div className={eventsCSS.cardImg}>
-                    <img alt="" src="imgPlaceholder.png" />
+                    <img alt="" src="mastermindstock.jpg" />
                 </div>
             </div>
         </div>
--- src/components/exco.js
+++ src/components/exco.js
@@ -7,32 +7,32 @@ const Exco = () => {
             <h3>2023 - PIONEER</h3>
             <div className={excoCSS.memberContainer}>
                 <div className={excoCSS.member}>
-                    <img src="./excoPlaceholder.png" alt=""/>
-                    <h3>Aaron Tua</h3>
+                    <img src="./person1.jpg" alt=""/>
+                    <h3>Aaron Tan</h3>
                     <p>President</p>
                 </div>
                 <div className={excoCSS.member}>
-                    <img src="./excoPlaceholder.png" alt=""/>
+                    <img src="./person2.jpg" alt=""/>
                     <h3>Lee Yan Han</h3>
                     <p>Vice President</p>
                 </div>
                 <div className={excoCSS.member}>
-                    <img src="./excoPlaceholder.png" alt=""/>
+                    <img src="./person3.jpg" alt=""/>
                     <h3>Justin Wong</h3>
                     <p>Head Of Outreach</p>
                 </div>
                 <div className={excoCSS.member}>
-                    <img src="./excoPlaceholder.png" alt=""/>
+                    <img src="./person4.jpg" alt=""/>
                     <h3>Matthias Lee</h3>
                     <p>Head Of Community</p>
                 </div>
                 <div className={excoCSS.member}>
-                    <img src="./excoPlaceholder.png" alt=""/>
+                    <img src="./person5.jpg" alt=""/>
                     <h3>Muthuvel Vimal</h3>
                     <p>Web3 Director</p>
                 </div>
                 <div className={excoCSS.member}>
-                    <img src="./excoPlaceholder.png" alt=""/>
+                    <img src="./person6.jpg" alt=""/>
                     <h3>Vivek Thomas</h3>
                     <p>Web3 Director</p>
                 </div>
